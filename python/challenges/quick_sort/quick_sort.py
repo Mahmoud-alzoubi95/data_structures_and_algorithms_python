ba9e6de4585d481f048e9987a852eb03044cf570
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-06-16 17:28:58 +0300

    add

--- python/challenges/quick_sort/quick_sort.py
+++ python/challenges/quick_sort/quick_sort.py
@@ -25,3 +25,9 @@ n = len(arr)
 quickSort(arr, 0, n-1)
 print("Sorted array is:")
 print(arr)
+
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+print(arr)
