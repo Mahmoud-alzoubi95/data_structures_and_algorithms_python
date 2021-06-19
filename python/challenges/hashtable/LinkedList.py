2f3aaba32c709572d7318375b476cddbf7d38374
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-06-19 18:42:31 +0300

    add linked list

---
+++ python/challenges/hashtable/LinkedList.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    def add(self,data):
+        node = Node(data)
+        if self.head :
+            current = self.head 
+            while current.next:
+                current=current.next
+            current=current.next
+        else:
+            self.head=node
+
+
+    def __str__(self) :
+        list = ""
+        current = self.head
+        while current :
+            list+=f"{current.data}--->"
+            current=current.next
+        list+="Null"
+
+        return list
+
+
+
+if __name__=='__main__':
+    X = LinkedList()
+    X.add('mahmoud')
+    X.add('hasan')
+    X.add('alzoubi')
+    print(X)
