edfa9e0151f6e2ddd4f318f032e4aa6539faba9d
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-06-20 20:41:01 +0300

    full solution

---
+++ python/challenges/repeated_word/README.md
@@ -0,0 +1,14 @@
+# Challenge Summary
+Write a function that accepts a lengthy string parameter.
+Without utilizing any of the built-in library methods available to your language, return the first word
+to occur more than once in that provided string.
+
+## Challenge Description
+define a function to split a string to array with the word of string then sort every word in hashtable it the value repeated return it directly
+
+
+## Approach & Efficiency
+ time: O(n)
+
+## Solution
+<img src="../../../assests/ch31.png">
---
+++ python/challenges/repeated_word/repeated_word.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+def lengthy_string(str):
+    txt=str
+    arr=splitFun(txt)
+    a = arr=[x.strip(',') for x in arr]
+    repeated = first_Repeated_Word(arr)
+    return repeated
+
+def splitFun(txt):
+    arr= txt.split()
+    return arr
+
+def first_Repeated_Word(arr):
+    counts=dict()
+    arr=tolower(arr)
+    for word in arr:
+        if word in counts:
+            counts[word]+=1
+            if counts[word]>1:
+                return word
+        else:
+            counts[word]=1
+    return None
+
+def tolower(arr):
+    return [x.lower() for x in arr]
+
+
+first=lengthy_string("Once upon a time, there was a brave princess who...")
+print(first)
+
+second=lengthy_string("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only")
+print(second)
+
+third=lengthy_string("It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..")
+print(third)
